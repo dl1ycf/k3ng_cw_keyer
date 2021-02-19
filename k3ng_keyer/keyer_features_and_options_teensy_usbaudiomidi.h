ba17b71ca6b2cc626e80a23991cd82175145bfb8
c vw <dl1ycf@darc.de>
2021-02-19 09:57:44 +0100

    Included options for Audio and MIDI settings

--- k3ng_keyer/keyer_features_and_options_teensy_usbaudiomidi.h
+++ k3ng_keyer/keyer_features_and_options_teensy_usbaudiomidi.h
@@ -1,6 +1,37 @@
 // compile time features and options - comment or uncomment to add or delete features
 // FEATURES add more bytes to the compiled binary, OPTIONS change code behavior
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Let's begin with some Teensy-MIDI-Audio specific options
+//
+//
+//#define OPTION_MIDI_CW_CHANNEL      5   // channel for sending key-down and ptt messages
+//#define OPTION_MIDI_CW_NOTE         1   // Note for key-up/down
+#define OPTION_MIDI_PTT_NOTE        2   // Note for PTT on/off
+//#define OPTION_MIDI_CONTROL_CHANNEL 2   // channel for receiving parameters
+#define OPTION_AUDIO_MQS                // define if using MQS audio out instead of I2S
+//#define OPTION_SIDETONE__VOLUME    0.2  // initial side tone volume
+//#define OPTION_SIDETONE_FREQ       800  // initial side tone frequency
+
+//
+// Notes:
+// ======
+//
+// If OPTION_MIDI_CW_CHANNEL      is not defined, channel 1 will be used
+// If OPTION_MIDI_CW_NOTE         is not defined, Note #1 is used for MIDI CW-key messages
+// If OPTION_MIDI_PTT_NOTE        is not defined, PTT MIDI messages are not sent
+// If OPTION_MIDI_CONTROL_CHANNEL is not defined, channel 2 will be listened to
+// If OPTION_AUDIO_MQS            is not defined the I2S audio device will be used
+// If OPTION_SIDETONE_VOLUME      is not defined 0.2 will be used
+// If OPTION_SIDETONE_FREQ        is not defined 600 Hz will be used
+//
+//
+// Now come the standard K3NG keyer option
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 // #define FEATURE_COMMAND_BUTTONS
 // #define FEATURE_COMMAND_LINE_INTERFACE  // Command Line Interface functionality
